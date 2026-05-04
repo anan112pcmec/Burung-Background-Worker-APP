72cce24d6e1f641f2deba3a1a105c6acea333bbb
Faiz Hannan <ananlol156@gmail.com>
2026-05-04 19:31:29 +0700

    alamat seller services handler done next barang seller

--- watcher_app/message_broker/dispatcher/seller/delete.go
+++ watcher_app/message_broker/dispatcher/seller/delete.go
@@ -1,7 +1,37 @@
 package consume_seller_dispatcher
 
-import mb_cud_serializer "github.com/anan112pcmec/Burung-backend-2/watcher_app/message_broker/serializer"
+import (
+	"github.com/anan112pcmec/Burung-backend-2/watcher_app/database/sot_database/models"
+	mb_cud_serializer "github.com/anan112pcmec/Burung-backend-2/watcher_app/message_broker/serializer"
+	alamat_seller_handle "github.com/anan112pcmec/Burung-backend-2/watcher_app/service_handle/seller_service/alamat_services"
+
+)
 
 func SellerDeleteServicesDispatcher[T mb_cud_serializer.ConsumeDataJson | mb_cud_serializer.ConsumeDataProto](data T) error {
+	var d mb_cud_serializer.ParsedDataMessage
+	switch v := any(data).(type) {
+	case mb_cud_serializer.ConsumeDataJson:
+		d = v.Parse()
+
+		switch d.TableName {
+		case models.AlamatGudang.TableName(models.AlamatGudang{}):
+			if err := alamat_seller_handle.DeleteHapusAlamatGudang(d); err != nil {
+				return err
+			}
+
+		}
+
+	case mb_cud_serializer.ConsumeDataProto:
+		d = v.Parse()
+
+		switch d.TableName {
+		case models.AlamatGudang.TableName(models.AlamatGudang{}):
+			if err := alamat_seller_handle.DeleteHapusAlamatGudang(d); err != nil {
+				return err
+			}
+
+		}
+	}
+
 	return nil
 }
---
+++ watcher_app/service_handle/seller_service/alamat_services/services_handle.go
@@ -0,0 +1,39 @@
+package alamat_seller_handle
+
+import (
+	"fmt"
+
+	"github.com/anan112pcmec/Burung-backend-2/watcher_app/database/sot_database/models"
+	"github.com/anan112pcmec/Burung-backend-2/watcher_app/helper"
+	mb_cud_serializer "github.com/anan112pcmec/Burung-backend-2/watcher_app/message_broker/serializer"
+)
+
+func CreateTambahAlamatGudang(Data mb_cud_serializer.ParsedDataMessage) error {
+	var Objek models.AlamatGudang
+	if err := helper.DecodeJSONBody(Data, &Objek); err != nil {
+		return fmt.Errorf("gagal mengolah data")
+	} else {
+		fmt.Println(Objek)
+	}
+	return nil
+}
+
+func UpdateEditAlamatGudang(Data mb_cud_serializer.ParsedDataMessage) error {
+	var Objek models.AlamatGudang
+	if err := helper.DecodeJSONBody(Data, &Objek); err != nil {
+		return fmt.Errorf("gagal mengolah data")
+	} else {
+		fmt.Println(Objek)
+	}
+	return nil
+}
+
+func DeleteHapusAlamatGudang(Data mb_cud_serializer.ParsedDataMessage) error {
+	var Objek models.Review
+	if err := helper.DecodeJSONBody(Data, &Objek); err != nil {
+		return fmt.Errorf("gagal mengolah data")
+	} else {
+		fmt.Println(Objek)
+	}
+	return nil
+}
