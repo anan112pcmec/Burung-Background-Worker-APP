e00588b05338a60cf8c3063b072d8e74c90acc94
Faiz Hannan <ananlol156@gmail.com>
2026-05-31 20:24:13 +0700

    selesai bikin alamat kurir services handle next informasi services handle

--- watcher_app/message_broker/dispatcher/kurir/delete.go
+++ watcher_app/message_broker/dispatcher/kurir/delete.go
@@ -1,7 +1,29 @@
 package consume_kurir_dispatcher
 
-import mb_cud_serializer "github.com/anan112pcmec/Burung-backend-2/watcher_app/message_broker/serializer"
+import (
+	"fmt"
+
+	"github.com/anan112pcmec/Burung-backend-2/watcher_app/database/sot_database/models"
+	mb_cud_serializer "github.com/anan112pcmec/Burung-backend-2/watcher_app/message_broker/serializer"
+	alamat_kurir_handle "github.com/anan112pcmec/Burung-backend-2/watcher_app/service_handle/kurir_service/alamat_services"
+)
 
 func KurirDeleteServicesDispatcher[T mb_cud_serializer.ConsumeDataJson | mb_cud_serializer.ConsumeDataProto](data T) error {
+	var d mb_cud_serializer.ParsedDataMessage
+	switch v := any(data).(type) {
+	case mb_cud_serializer.ConsumeDataJson:
+		d = v.Parse()
+	case mb_cud_serializer.ConsumeDataProto:
+		d = v.Parse()
+	default:
+		return fmt.Errorf("unsupported data type")
+	}
+
+	switch d.TableName {
+	case models.AlamatKurir{}.TableName():
+		if err := alamat_kurir_handle.DeleteHapusAlamatKurir(d); err != nil {
+			return err
+		}
+	}
 	return nil
 }
---
+++ watcher_app/service_handle/kurir_service/alamat_services/services_handle.go
@@ -0,0 +1,39 @@
+package alamat_kurir_handle
+
+import (
+	"fmt"
+
+	"github.com/anan112pcmec/Burung-backend-2/watcher_app/database/sot_database/models"
+	"github.com/anan112pcmec/Burung-backend-2/watcher_app/helper"
+	mb_cud_serializer "github.com/anan112pcmec/Burung-backend-2/watcher_app/message_broker/serializer"
+)
+
+func CreateMasukanAlamatKurir(Data mb_cud_serializer.ParsedDataMessage) error {
+	var DataAlamatKurir models.AlamatKurir
+	if err := helper.DecodeJSONBody(Data, &DataAlamatKurir); err != nil {
+		return fmt.Errorf("gagal mengolah data alamat")
+	}
+
+	fmt.Println("Berhasil mendapatkan data", DataAlamatKurir.IdKurir)
+	return nil
+}
+
+func UpdatedEditAlamatKurir(Data mb_cud_serializer.ParsedDataMessage) error {
+	var DataAlamatKurir models.AlamatKurir
+	if err := helper.DecodeJSONBody(Data, &DataAlamatKurir); err != nil {
+		return fmt.Errorf("gagal mengolah data alamat")
+	}
+
+	fmt.Println("Berhasil mendapatkan data", DataAlamatKurir.IdKurir)
+	return nil
+}
+
+func DeleteHapusAlamatKurir(Data mb_cud_serializer.ParsedDataMessage) error {
+	var DataAlamatKurir models.AlamatKurir
+	if err := helper.DecodeJSONBody(Data, &DataAlamatKurir); err != nil {
+		return fmt.Errorf("gagal mengolah data alamat")
+	}
+
+	fmt.Println("Berhasil mendapatkan data", DataAlamatKurir.IdKurir)
+	return nil
+}
